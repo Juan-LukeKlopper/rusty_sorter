2639ff30c6983284f612d3679be3233ccfd815b0
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-09-06 18:22:52 +0200

    feat: add struct and impl the fn as a trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
 wai_bindgen_rust::export!("rusty-sorter.wai");
+
+pub struct RustySorter;
+
+impl rusty_sorter::RustySorter for RustySorter {
+    fn bubble_sort(a:Vec<i32>,) -> Vec<i32> {
+        let mut unsorted_array = a.clone();
+        for i in 0..a.len() {
+            for j in 0..(a.len() - i - 1) {
+                if unsorted_array[j] > unsorted_array[j + 1] {
+                    unsorted_array.swap(j, j+1);
+                }
+            }
+        }
+
+        unsorted_array
+    }
+}
